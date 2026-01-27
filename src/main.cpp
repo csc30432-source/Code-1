6939d582737df625cd168e2bc419ebb2b488c719
Cameron Cheek <csc30432@email.vccs.edu>
2026-01-27 17:34:12 -0500

    Initial Commit for program

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// This file is for the source code that
+// you will be asked to provide in the instructions.
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, World!" << endl;
+  return 0;
+}
