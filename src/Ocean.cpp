3ef8abd6da86a5a57272845b2c6448a9c63faaed
Cameron Cheek <csc30432@email.vccs.edu>
2026-02-11 12:49:13 -0500

    ocean.complete

---
+++ src/Ocean.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const double rise_rate = 1.5;
+
+  cout << "in 5 Years: " << (rise_rate * 5) << " mm" << endl;
+  cout << "in 7 Years: " << (rise_rate * 7) << " mm" << endl;
+  cout << "in 10 Years: " << (rise_rate * 10) << " mm" << endl;
+
+  return 0;
+}
